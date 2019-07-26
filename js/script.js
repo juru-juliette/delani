d963c50cee32565d00cbd35377b3f110899bf8d8
juru-juliette <kangabejuliette@gmail.com>
2019-07-26 17:27:52 +0200

    adding toggle on paragraph

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $("img#icon1").click(function(){
+        $("#P1").toggle();
+ });
+ $("img#icon2").click(function(){
+    $("#P2").toggle();
+ });
+ $("img#icon3").click(function(){
+    $("#P3").toggle();
+ });
+});
