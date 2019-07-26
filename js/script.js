922cd387fa236343c7825e9e9dbc882185793e9d
juru-juliette <kangabejuliette@gmail.com>
2019-07-26 18:29:43 +0200

    I created project name

--- js/script.js
+++ js/script.js
@@ -5,18 +5,9 @@ $(document).ready(function(){
  $("img#icon2").click(function(){
     $("#P2").toggle();
  });
+
  $("img#icon3").click(function(){
     $("#P3").toggle();
  });
- $(function(){
-    $("#img2").on({
-     mouseenter: function(){
-      $(this).attr('src','images/work4.jpg');
-    },
-    mouseleave: function(){
-      $(this).attr('src','images/work4.jpg');
-    }
-    });
-    
-  });
-});
+ });
+ 
