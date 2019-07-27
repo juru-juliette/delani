1e0d62c9851d4bd7fa33646e93ddd260d2fc5f48
juru-juliette <kangabejuliette@gmail.com>
2019-07-27 16:19:27 +0200

    I created another function for hovering

--- js/script.js
+++ js/script.js
@@ -9,6 +9,15 @@ $(document).ready(function(){
  $("img#icon3").click(function(){
     $("#P3").toggle();
  });
- 
+ $(".one").hide();
+ $("#col1").animate({opacity:1});
+ $("#img2").hover(function(){
+    $(this).stop().animate({opacity:.4},200);
+    $(".one").fadeIn();
+ },
+ function(){
+    $(this).stop().animate({opacity:1},500);
+    $(".one").fadeOut();
+ });
  });
  
